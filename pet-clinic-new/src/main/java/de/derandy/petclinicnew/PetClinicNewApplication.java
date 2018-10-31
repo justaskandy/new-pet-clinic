7a664fc5eb5f68210f57422c62336c643e74078c
anhev <anhev@andy>
2018-10-31 12:54:40 +0100

    initial commit

---
+++ pet-clinic-new/src/main/java/de/derandy/petclinicnew/PetClinicNewApplication.java
@@ -0,0 +1,12 @@
+package de.derandy.petclinicnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicNewApplication.class, args);
+	}
+}
---
+++ pet-clinic-new/src/test/java/de/derandy/petclinicnew/PetClinicNewApplicationTests.java
@@ -0,0 +1,16 @@
+package de.derandy.petclinicnew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinicNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
